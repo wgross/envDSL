aca7bfa3714c884b28dec861392fa51d4bb695cd
Wolfgang Groß <bitbucket@text-mode.de>
2016-08-23 11:37:53 +0200

    Test-AdminUser and Add-EnvPath tested

--- 2.0/envDSL.Test.ps1
+++ 2.0/envDSL.Test.ps1
@@ -54,3 +54,74 @@ Describe "powershellHost DSL Item" {
         $script:wasCalled | Should Be $false
     }
 }
+
+Describe "Add-EnvPathVariable" {
+    
+    BeforeAll {
+        Remove-Item "Env:\samplePath" -ErrorAction SilentlyContinue
+    }
+
+    It "Adds a directory at the end of an empty variable" {
+        $env:SamplePath = $null
+        Add-EnvPath -Path "Env:\samplePath" -Append "TestDrive:\test"
+        $env:samplePath | Should Be "TestDrive:\test"
+    }
+    
+    It "Adds a directory at the end of an non-empty variable" {
+        $env:SamplePath = "C:\another\path"
+        Add-EnvPath -Path "Env:\samplePath" -Append "TestDrive:\test"
+        $env:samplePath | Should Be "C:\another\path;TestDrive:\test"
+    }
+
+    It "Adds a multiple directories at the end of the specified order of an non-empty variable" {
+        $env:SamplePath = "C:\another\path"
+        Add-EnvPath -Path "Env:\samplePath" -Append "D:\some\path","TestDrive:\test"
+        $env:samplePath | Should Be "C:\another\path;D:\some\path;TestDrive:\test"
+    }
+
+    It "Moves a directory at the end if it is appended a second time" {
+        $env:SamplePath = "D:\first;C:\another\path"
+        Add-EnvPath -Path "Env:\samplePath" -Append "D:\first"
+        $env:samplePath | Should Be "C:\another\path;D:\first"
+    }
+
+    It "Adds a directory at the beginning of an empty variable" {
+        $env:SamplePath = $null
+        Add-EnvPath -Path "Env:\samplePath" -Prepend "TestDrive:\test"
+        $env:samplePath | Should Be "TestDrive:\test"
+    }
+
+    It "Adds a directory at the begining of an non-empty variable" {
+        $env:SamplePath = "C:\another\path"
+        Add-EnvPath -Path "Env:\samplePath" -Prepend "TestDrive:\test"
+        $env:samplePath | Should Be "TestDrive:\test;C:\another\path"
+    }
+
+    It "Adds a multiple directories at the beginning of the specified order of an non-empty variable" {
+        $env:SamplePath = "C:\another\path"
+        Add-EnvPath -Path "Env:\samplePath" -Prepend "D:\some\path","TestDrive:\test"
+        $env:samplePath | Should Be "D:\some\path;TestDrive:\test;C:\another\path"
+    }
+
+    It "Moves a directory at the Beginning if it is prepended a second time" {
+        $env:SamplePath = "C:\another\path;E:\last"
+        Add-EnvPath -Path "Env:\samplePath" -Prepend "E:\last"
+        $env:samplePath | Should Be "E:\last;C:\another\path"
+    }
+} 
+
+Describe "Test-AdminUser" {
+    
+    It "Doesn't execute a script blo$ck if the current user isn't an admin User" {
+        $script:wasExecuted = $false
+        Test-AdminUser | Should Be $false
+    }
+
+    It "Throws instead of returning false if an error test text is given" {
+        try {
+            Test-AdminUser -Throw "Not Admin"
+        } catch {
+            $_.ToString() | Should Be "Not Admin"
+        }
+    }
+}
--- 2.0/envDSL.psm1
+++ 2.0/envDSL.psm1
@@ -112,3 +112,83 @@ function powershellHost {
 }
 
 #endregion 
+
+#region Add-EnvPathVariable
+
+function Add-EnvPath {
+    <#
+    .SYNOPSIS
+        Adds the specified pathes to an environment variable if it doesn't already contain the pathes.
+    .DESCRIPTION
+        If an array is specified the relative order is optained. 
+    #>
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Path,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [string[]]$Prepend,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [string[]]$Append
+    )
+    process {
+        
+        # split the current content into a list of entries. Pathes are seperated with ';'
+        [System.Collections.Generic.List[string]]$splitted = [System.Collections.Generic.List[string]]::new()
+        if(Test-Path $Path) {
+            [System.Collections.Generic.List[string]]$splitted = [System.Linq.Enumerable]::ToList((Get-Content -Path $Path) -split ";")
+        }
+
+        if($Append) {
+            $Append | ForEach-Object {
+                if($splitted.Contains($_)) {
+                    $splitted.Remove($_) | Out-Null
+                    Write-Verbose "Removed before adding $_"
+                }
+                if($splitted -notcontains $_) {
+                    $splitted.Add($_) | Out-Null
+                    Write-Verbose "Added at the end $_"
+                }
+            }
+        }
+        if($Prepend) {
+            # Revert the array to start prepending with the last element
+            # ==> The relative order is obtained because the first item is prepended as the last and is 
+            #     therefore the first in the result path
+            [array]::Reverse($Prepend)
+            $Prepend | ForEach-Object {
+                if($splitted.Contains($_)) {
+                    $splitted.Remove($_) | Out-Null
+                    Write-Verbose "Removed before adding $_"
+                }
+                if($splitted -notcontains $_) {
+                    $splitted.Insert(0,$_) | Out-Null
+                    Write-Verbose "Added in front $_"
+                }
+            } 
+        }
+        Set-Content -Path $Path -Value ([string]::Join(";",($splitted | Where-Object { ![string]::IsNullOrWhiteSpace($_) })))
+    }
+}
+#endregion 
+
+#region Test-AdmnUser
+
+function Test-AdminUser {
+    param(
+        [Parameter(Mandatory=$false)]
+        $Throw
+    )
+    process {
+        $isInRole = (New-Object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+        if($PSBoundParameters.ContainsKey("Throw")) {
+            throw ($Throw)
+        }
+        return $isInRole
+    }
+}
+ 
+#enregion 
